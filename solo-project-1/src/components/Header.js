012ddd276bee93860d86307c3ece473ed229d49a
Matas Pigaga <tech@matas.io>
2021-11-13 15:47:00 -0500

    almost done / working on footer

--- solo-project-1/src/components/Header.js
+++ solo-project-1/src/components/Header.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Header() {
-  return <h1>header</h1>;
+  return (
+    <section className="header">
+      <h1>Matas Pigaga</h1>
+      <h2>Web Developer</h2>
+      <span>matas.io</span>
+      <div className="header--buttons">
+        <a href="mailto:hello@matas.io">Email</a>
+        <a href="https://twitter.com/mataspigaga">Twitter</a>
+      </div>
+    </section>
+  );
 }
--- solo-project-1/src/components/Info.js
+++ solo-project-1/src/components/Info.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Info() {
-  return <h1>info</h1>;
+  return (
+    <main className="info">
+      <section className="info--section">
+        <h3>About</h3>
+        <p>
+          I am an aspiring web dveloper with a particular interest in
+          optimization. I like building things from the ground up with
+          performance and user experience in mind.
+        </p>
+      </section>
+      <section className="info--section">
+        <h3>Interests</h3>
+        <p>
+          Party DJ. Audiobook Addict. Car Aficionado. Entreprenuer. Coffee Shop
+          Crawler. Cat Dad of 2. Bartender.
+        </p>
+      </section>
+    </main>
+  );
 }
