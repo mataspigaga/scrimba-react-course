5a29bd8268ccc254c480c708796404645ab1e485
Matas Pigaga <tech@matas.io>
2021-11-14 14:56:10 -0500

    used yarn to install react icons

--- solo-project-1/src/components/Image.js
+++ solo-project-1/src/components/Image.js
@@ -2,5 +2,5 @@ import React from "react";
 import mainPhoto from '../assets/avatar.png'
 
 export default function Image() {
-  return <img src={mainPhoto} alt="matas pigaga headshot photo" className='main--photo' />;
+  return <img src={mainPhoto} alt="matas pigaga headshot" className='main--photo' />;
 }
