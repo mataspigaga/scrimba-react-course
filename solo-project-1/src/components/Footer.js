a0675aa9ac74cb83868719e75d47f0769ae418d1
Matas Pigaga <tech@matas.io>
2021-11-14 14:21:03 -0500

    finished, going to change the buttons though

--- solo-project-1/src/components/Footer.js
+++ solo-project-1/src/components/Footer.js
@@ -2,5 +2,20 @@ import React from "react";
 import { SiInstagram, SiTwitter, SiGmail, SiGithub } from "react-icons/si";
 
 export default function Footer() {
-  return <h1>footer</h1>;
+  return (
+    <footer className='footer--social'>
+      <a href="https://www.twitter.com/mataspigaga/">
+        <SiTwitter />
+      </a>
+      <a href="https://www.instagram.com/mataspigaga/">
+        <SiInstagram />
+      </a>
+      <a href="mailto:hello@matas.io">
+        <SiGmail />
+      </a>
+      <a href="https://www.github.com/mataspigaga/">
+        <SiGithub />
+      </a>
+    </footer>
+  );
 }
