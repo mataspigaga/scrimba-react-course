8fbcd7c9890cbb73570582c3925e207917e7962a
Matas Pigaga <tech@matas.io>
2021-11-19 14:57:17 -0500

    fixed data name

--- airbnb-experiences-clone/src/assets/data.js
+++ airbnb-experiences-clone/src/assets/data.js
@@ -1,4 +1,4 @@
-export default [
+const cardData = [
   {
     id: 1,
     title: "Life Lessons with Katie Zaferes",
@@ -42,3 +42,5 @@ export default [
     openSpots: 3,
   },
 ];
+
+export default cardData;
