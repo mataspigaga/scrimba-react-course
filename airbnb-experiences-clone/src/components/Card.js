0f2c90e43c3f5551c6acaf01e0c64a3eabe2a4a4
Matas Pigaga <tech@matas.io>
2021-11-19 15:31:38 -0500

    let's try this image fix...

--- airbnb-experiences-clone/src/components/Card.js
+++ airbnb-experiences-clone/src/components/Card.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import image from "../assets/katie-zaferes.png";
 
 export default function Card(props) {
+  console.log(image);
   return (
     <section className="card--container">
       <a href="/" className="card--image">
         {props.openSpots <= 0 && <span>Sold Out</span>}
         {/* <img src={require(`../../pubic/${props.img}`)} alt={props.title} /> */}
-        <img src={require(`../assets/${props.img}.default`)} alt={props.title} />
+        <img src={`../assets/${props.img}`.toString()} alt={props.title} />
       </a>
       <div className="card--info">
         <div className="card--info--ratings">
