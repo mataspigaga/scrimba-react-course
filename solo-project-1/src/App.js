8ce2b00fa2fb58098affd4095ca7d12388b42512
Matas Pigaga <tech@matas.io>
2021-11-20 13:46:30 -0500

    #100DaysOfCode counter is now dynamic

--- solo-project-1/src/App.js
+++ solo-project-1/src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Image from "./components/Image";
-import Info from "./components/Info";
+import BusinessCard from "./components/BusinessCard";
 import "./style.css";
 
 export default function App() {
   return (
     <div className="container">
-      <Image />
-      <Header />
-      <Info />
-      <Footer />
+      <BusinessCard />
     </div>
   );
 }
--- solo-project-1/src/components/Header.js
+++ solo-project-1/src/components/Header.js
@@ -2,13 +2,22 @@ import React from "react";
 import { SiAngellist } from "react-icons/si";
 
 export default function Header() {
+  const daysPassed =
+    61 +
+    Math.floor(
+      (new Date() - new Date("November 20, 2021 00:00:00")) /
+        (24 * 60 * 60 * 1000)
+    );
+
   return (
     <section className="header">
       <h1>Matas Pigaga</h1>
       <h2>Web Developer</h2>
       <span>Check out my #100DaysOfCode blog:</span>
       <span>
-        <a href="https://mataspigaga.com/posts/100-days">Day 56/100</a>
+        <a href="https://mataspigaga.com/posts/100-days">
+          Day {daysPassed}/100
+        </a>
       </span>
       <div className="header--button">
         <a href="https://twitter.com/mataspigaga">
