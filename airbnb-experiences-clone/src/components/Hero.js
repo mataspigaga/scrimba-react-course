6489126b6b100f9492fcb9a1a98b41e60024da4e
Matas Pigaga <tech@matas.io>
2021-11-28 14:38:58 -0500

    updated hero

--- airbnb-experiences-clone/src/components/Hero.js
+++ airbnb-experiences-clone/src/components/Hero.js
@@ -4,14 +4,11 @@ import headerImage from "../assets/photo-grid.png";
 export default function Hero() {
   return (
     <section className="hero--container">
-      <img
-        src={headerImage}
-        alt="experiences collage"
-      />
-      <h1>Online Experiences</h1>
+      <img src={headerImage} alt="experiences collage" />
+      <h1>Unique Experiences</h1>
       <p>
-        Join unique interactive activities led by one-of-a-kind hosts—all
-        without leaving home.
+        Join others in totally normal activities for affordable prices. If
+        you've experienced injury and or death, please contact a lawyer.
       </p>
     </section>
   );
