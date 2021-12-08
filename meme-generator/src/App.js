6a9273cb680bb8949a4e6f82cefe28f0203d166b
Matas Pigaga <tech@matas.io>
2021-12-08 13:33:56 -0500

    random meme images rendered / styled

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Header from "./components/Header";
-import Meme from "./components/Meme";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Form />
-      <Meme />
       <Footer />
     </div>
   );
--- meme-generator/src/components/Form.js
+++ meme-generator/src/components/Form.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../assets/memesData";
 
 export default function Form() {
+  const [memeImage, setMemeImage] = useState(
+    "https://placekitten.com/1000/1000"
+  );
+
+  const memeHandler = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMemeImage(url);
+  };
+
   return (
     <section className="form--container">
       <form className="input--container">
         <input placeholder="Top Text" type="text" className="top--text" />
         <input placeholder="Bottom Text" type="text" className="bottom--text" />
       </form>
-      <button className="form--button">
+      <button className="form--button" onClick={memeHandler}>
         <span>Get a new meme image</span>
       </button>
+      <img className="meme--image" src={memeImage} alt="placeholder" />
     </section>
   );
 }
