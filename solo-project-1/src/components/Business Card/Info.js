537d6515533d7394773f40c9c414a05ad28f44a8
Matas Pigaga <tech@matas.io>
2022-02-09 14:38:16 -0500

    added minimal portfolio link

--- solo-project-1/src/components/Business Card/Info.js
+++ solo-project-1/src/components/Business Card/Info.js
@@ -20,6 +20,15 @@ export default function Info() {
       </section>
       <section className="info--section">
         <h3>Projects</h3>
+        {/* New Portfolio */}
+        <a
+          className="info--project"
+          href="https://project-portfolio-a14d17.webflow.io/"
+        >
+          <h4 className="info--project--0">
+            <strong>New Minimal Portfolio</strong>
+          </h4>
+        </a>
         {/* Project 1 */}
         <a
           className="info--project"
@@ -58,12 +67,7 @@ export default function Info() {
           className="info--project"
           href="https://tenzies-game-seven.vercel.app/"
         >
-          <h4 className="info--project--5">
-            Tenzies Game -{" "}
-            <em>
-              <strong>In Progress</strong>
-            </em>
-          </h4>
+          <h4 className="info--project--5">Tenzies Game</h4>
         </a>
       </section>
     </main>
